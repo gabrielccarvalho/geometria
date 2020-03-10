f108581350d6b9f2ce415bab0747eb9e37d690e6
GABRIEL CAMPOS DOS SANTOS PECANHA DE CARVALHO <campos10@A305-36L.imd.local>
2020-03-10 16:07:14 -0300

    Adicionado Comentários no arquivo .h

--- src/lib/geometria.h
+++ src/lib/geometria.h
@@ -1,3 +1,7 @@
+// Função para calcular área e perímetro de figuras planas.
+// Recebe como parâmetro a figura e suas medidas.
 void plano(int argc, char* argv[]);
 
+// Função para calcular área e volume de figuras espaciais.
+// Recebe como parâmetro a figura e suas medidas.
 void espacial(int argc, char* argv[]);
